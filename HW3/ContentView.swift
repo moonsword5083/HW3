763b17a8631d0804a2d7fa0c036026b7ab392c4f
c8610290927 <c8610290927@gmail.com>
2020-05-03 21:01:02 +0800

    Initial Commit

---
+++ HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3
+//
+//  Created by User20 on 2020/5/3.
+//  Copyright © 2020 00657143. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
